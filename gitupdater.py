51a025d466c87e1e9483076186b535e50c0aabf8
Cs0k1ka <palotaikrili@gmail.com>
2025-09-19 23:41:39 +0200

    Updated the pathfinding for commits and pulls.

--- gitupdater.py
+++ gitupdater.py
@@ -31,7 +31,7 @@ def menu():
 # Commit changes to the desired repository
 def commit_changes():
     repo_location = input("Where is your repository located? >> ")
-    repo_path = os.path.abspath(os.path.join(os.getcwd(), "..", repo_location))
+    repo_path = os.path.abspath(os.path.join(os.getcwd(), repo_location))
 
     if not os.path.isdir(repo_path):
         print("Error: folder not found!", repo_path)
@@ -43,15 +43,15 @@ def commit_changes():
     file_selector = input("What files would you like to add? (e.g., script.js) or '.' for all >> ")
     commit_comment = input("What's your commit comment? >> ")
 
-    subprocess.run(["git", "add", file_selector], cwd=repo_path)
-    subprocess.run(["git", "commit", "-m", commit_comment], cwd=repo_path)
-    subprocess.run(["git", "push"], cwd=repo_path)
+    subprocess.run(["git", "add", file_selector], cwd = repo_path)
+    subprocess.run(["git", "commit", "-m", commit_comment], cwd = repo_path)
+    subprocess.run(["git", "push"], cwd = repo_path)
     print("Commit and push completed.")
 
 # Pull the content of an updated repository
 def pull_repo():
     repo_location = input("Where is your repository located? >> ")
-    repo_path = os.path.abspath(os.path.join(os.getcwd(), "..", repo_location))
+    repo_path = os.path.abspath(os.path.join(os.getcwd(), repo_location))
 
     if not os.path.isdir(repo_path):
         print("Error: folder not found!", repo_path)
@@ -60,7 +60,7 @@ def pull_repo():
         print("Error: folder is not a Git repository!", repo_path)
         return
 
-    subprocess.run(["git", "pull"], cwd=repo_path)
+    subprocess.run(["git", "pull"], cwd = repo_path)
     print("Pull completed.")
 
 # Clone a repository easily
